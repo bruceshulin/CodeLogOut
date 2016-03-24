e893482ec3aa0dcaddc17fab3cbcf37c253c22b7
bruce <452113521>
2016-03-25 00:39:29 +0800

    区块分析制做，好像不可行，下回尝试列表方式

--- CodeLogOut/Form1.cs
+++ CodeLogOut/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CodeLogOut
 {
@@ -29,7 +30,13 @@ namespace CodeLogOut
 
         private void btnOutFile_Click(object sender, EventArgs e)
         {
-            //
+            //格式化文本 这一要第三方软件多得是，就不参与设计了，以免出错
+            //加主指定的log
+            //通过全局搜索{} 然后判断里面还有没有{}  直到最里层，然后只在最里层或代码层输出log
+            StringBuilder sbContent = new StringBuilder();
+            string codeContent = System.IO.File.ReadAllText(this.txtFile.Text);
+
         }
+     
     }
 }
